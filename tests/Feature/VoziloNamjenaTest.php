b019c046f86e60785292dd0dbc613067dbe39f2f
mkrivic83 <krivic.milan@gmail.com>
2026-02-02 13:02:59 +0100

    Revert "Add GitHub Actions CI (MySQL + tests)"
    This reverts commit 9cd01eaf681a7e4f0368a2d983e03af02739601a.

--- tests/Feature/VoziloNamjenaTest.php
+++ tests/Feature/VoziloNamjenaTest.php
@@ -38,7 +38,7 @@ class VoziloNamjenaTest extends TestCase
 
         // 4️⃣ Provjere
         $this->assertNotNull($vozilo->namjena);
-        $this->assertEquals('Osobno', $vozilo->namjena->id);
+        $this->assertEquals('Osobno', $vozilo->namjena->naziv);
     }
 
     /** @test */
